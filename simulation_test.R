1e344f166c64b9020c212a155e7fc2af6591098d
abhijatri <statjatri@.com>
2025-05-20 23:50:41 +0530

    Iteration 2

--- simulation_test.R
+++ simulation_test.R
@@ -4,69 +4,67 @@ library(dplyr)
 library(MASS)
 library(ggplot2)
 library(mvtnorm)
+
 df <- Canada
 
-
-a <- VAR(y = df, p = 2, type = "const")
-
-summary(a)
-
-VARselect(y = df)
-
-summary(a)
-
-data(Canada) 
-var.2c <- VAR(Canada, p = 2, type = "const") 
-## Restrictions determined by thresh 
-d <- restrict(var.2c, method = "ser") 
-
-summary(var.2c)
-coefficients(d)
-
-fanchart(predict(d, n.ahead = 5))
-
-fanchart(predict(var.2c, n.ahead = 10000))
-
-
-## Restrictions set manually 
-restrict <- matrix(c(1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0), nrow=4, ncol=9, byrow=TRUE) 
-a <- restrict(var.2c, method = "man", resmat = restrict)
-
-
-coeff <- coefficients(a)
-varcov_resid <- summary(a)$covres
-
-names(c)
-
-sapply(names(c), function(x) {
-  x <- c[[x]][,1]
-})
-
-x2 <- bind_rows(c[["e"]][,1],c[["prod"]][,1], c[["rw"]][,1], c[["U"]][,1])
-
-x2 <- rep(0, 4)
+#check what provides the optimum lag of endogeneous variables
+varselect <- VARselect(y = df)
+print(varselect)
 
 p <- 2
-coeff_mat <- rep(0, 1 + p*length(names(coeff)))
-names(coeff_mat) <- c("const", paste0(names(coeff),".l1"), paste0(names(coeff),".l2"))
-
-coeff_mat <- bind_rows(coeff_mat, do.call('bind_rows', sapply(names(coeff), function(x){
-  coeff[[x]][,1]
-})))
-
-coeff_mat[is.na(coeff_mat)] <- 0
-coeff_mat <- data.frame(coeff_mat)
-rownames(coeff_mat) <- names(coeff)
-
+type <- "restrict"
+restrict_type <- "ser"
 nsim <- 10000
 burn_in_period <- 30
+forecast_start_data <- as.matrix(df[(r - p + 1):r,])
+relative_lags <- c(0,0,0,0)
+
+
+var.2c <- VAR(df, p = p, type = "const")
+
+if (is.null(type) == FALSE & is.null(restrict_type) == FALSE) {
+  if (type == "restrict" & restrict_type == "ser") {
+    a <- restrict(var.2c, method = restrict_type, thresh = thresh) 
+  } else if (type == "restrict" & restrict_type == "man") {
+    a <- restrict(var.2c, method = restrict_type, resmat = restrict_mat)  
+  }} else if (is.null(type) == TRUE & is.null(restrict_type) == TRUE) {
+    a <- var.2c
+  } else {
+    message("type and restrict_type combination not appropriately entered, will go ahead with the unrestricted VAR model")
+    a <- var.2c
+  }
+
+#collating the VAR model coefficients
+coeff <- coefficients(a)
+
+#storing the variance covariance matrix of the residuals to be used for drawing random samples from mvtnorm in the simulation
+varcov_resid <- summary(a)$covres
+
+coeff_mat <- rep(0, 1 + p*length(names(coeff)))
+
+nv <- length(names(coeff))
+names(coeff_mat)[1] <- "const"
+for (i in 1:p) {
+  names(coeff_mat)[((i-1)*nv + 2): (i*nv + 1)] <- paste0(names(coeff),".l",i)
+}
+
+#create a generic coefficient matrix
+coeff_mat <- bind_rows(coeff_mat, do.call('bind_rows', sapply(names(coeff), function(x){
+  coeff[[x]][,1]
+}, simplify = F)))
+
+coeff_mat[is.na(coeff_mat)] <- 0
+coeff_mat <- data.frame(coeff_mat)[-1,]
+rownames(coeff_mat) <- names(coeff)
+
+#define the forecast matrix
 forecast_mat <- matrix(NA, nrow = length(names(coeff)), ncol = burn_in_period + p)
 rownames(forecast_mat) <- names(coeff)
 
 r <- nrow(df)
 nv <- length(names(coeff))
 
-forecast_mat[,1:p] <- t(df[(r - p + 1):r,])
+forecast_mat[,1:p] <- t(forecast_start_data)
 
 forecast_mat <- as.matrix(forecast_mat)
 
@@ -74,7 +72,7 @@ forecast_mat <- as.matrix(forecast_mat)
 #simulation
 
 sim_list <- sapply(1:nsim, function(x) {
-  
+  message(paste("Simulation",x))
   #ith simulation
   for (j in (p+1):(burn_in_period + p)){
     forecast_mat[,j] <- coeff_mat[["const"]] + rmvnorm(1, sigma = varcov_resid)
@@ -86,13 +84,19 @@ sim_list <- sapply(1:nsim, function(x) {
   forecast_mat 
 }, simplify = F)
 
-relative_lags <- rep(c(0,2,2,2), nv)
 #select the simulations
 sim_obs <- data.frame(do.call('rbind',sapply(1:nsim, function(x){
   sapply(1:nv, function(y){
     t(sim_list[[x]][,(p + burn_in_period - relative_lags[y])])[,y]
   })
 }, simplify = F)))
+
+
+#collating statistics between actuals and simulated
+stat_list <- list(mean = list(actual = apply(df,2, mean), simulated = apply(sim_obs,2,mean)),
+                  sd = list(actual = apply(df,2, sd), simulated = apply(sim_obs,2,sd)),
+                  varcov = list(actual = var(df), simulated = var(sim_obs)),
+                  corr =list(actual = cor(df), simulated = cor(sim_obs)))
 
 #plot simulated vs actual density chart
 library(reshape2)
@@ -106,7 +110,10 @@ act_sim_obs <- melt(act_sim_obs, id.vars = c("data_type"))
 ggplot(data = act_sim_obs, aes(x = value, colour = data_type)) + 
   stat_density(geom = "line") + facet_wrap(variable ~ ., scales = "free") + 
   theme_bw() + labs(x = "", y = "Density", colour = "", title = "Density charts of Macro Economic Indiactors",
-                    subtitle = "Actual vs Simulated") +
+                    subtitle = paste0("Actual vs Simulated (",nsim,")")) +
   theme(legend.position = "bottom", panel.grid = element_blank())
+
+
+
 
 #plotting burn-in period charts
