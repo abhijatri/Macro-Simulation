a877b428f4a92255464447f6cf1aa2ad53ad174c
abhijatri <statjatri@.com>
2025-05-21 12:00:22 +0530

    minor change

--- simulation_test.R
+++ simulation_test.R
@@ -7,9 +7,9 @@ library(mvtnorm)
 library(reshape2)
 
 
-var_simulation <- function(df, p = 1, type = NULL, restrict_type = NULL, thresh = 2,
-                            nsim = 100, burn_in_period = 30,
-                            forecast_start_data = NULL, relative_lags = NULL) {
+var_simulation <- function(df, p = 1, type = NULL, restrict_type = NULL, thresh = 2, restrict_mat = NULL,
+                           nsim = 100, burn_in_period = 30,
+                           forecast_start_data = NULL, relative_lags = NULL, varcov_mat = NULL) {
   
   r <- nrow(df)
   nv <- ncol(df)
@@ -43,7 +43,12 @@ var_simulation <- function(df, p = 1, type = NULL, restrict_type = NULL, thresh 
   coeff <- coefficients(a)
   
   #storing the variance covariance matrix of the residuals to be used for drawing random samples from mvtnorm in the simulation
-  varcov_resid <- summary(a)$covres
+  if (is.null(varcov_mat) == TRUE) {
+    varcov_resid <- summary(a)$covres
+  } else {
+    varcov_resid <- varcov_mat
+  }
+  
   
   coeff_mat <- rep(0, 1 + p*length(names(coeff)))
   
@@ -96,10 +101,6 @@ var_simulation <- function(df, p = 1, type = NULL, restrict_type = NULL, thresh 
     })
   }, simplify = F)))
   
-  
-  #collating statistics between actuals and simulated
-  
-  
   #plot simulated vs actual density chart
   df2 <- data.frame(df)
   sim_obs2 <- data.frame(sim_obs)
@@ -115,33 +116,106 @@ var_simulation <- function(df, p = 1, type = NULL, restrict_type = NULL, thresh 
                       subtitle = paste0("Actual vs Simulated (",nsim,")")) +
     theme(legend.position = "bottom", panel.grid = element_blank())
   
-  return_list <- list(model_summary = summary(a),
+  #variance decomposition plot
+  d <- fevd(a, n.ahead = burn_in_period)
+  
+  d <- do.call('rbind', sapply(names(coeff), function(x){
+    f <- data.frame(d[[x]])
+    f$var <- x
+    f$t <- c(1:burn_in_period)
+    f
+  }, simplify = F))
+  
+  d <- melt(d, id.vars = c("var", "t"))
+  
+  var_decomp_plot <- ggplot(data = d, aes(x = t, y = value, fill = variable)) + 
+    geom_bar(stat = "identity") + facet_wrap(var ~ .) + 
+    scale_fill_brewer(palette = 7, direction = 1)+ 
+    theme_bw() + theme(legend.position = "bottom", panel.grid = element_blank()) + 
+    labs(title = "Variance Decomposition (%)", x = "Forecast Length (t)", y = "% of Variance", fill = "")
+  
+  #plot burn-in-period analysis
+  d <- do.call('rbind', sapply(1:nsim, function(x) {
+    k <- data.frame(t(sim_list[[x]]))[-c(1:p),]
+    k$sim_no <- x
+    k$t <- as.numeric(row.names(k)) - p
+    k
+    
+  }, simplify = F))
+  
+  d <- melt(d, id.vars = c("sim_no","t"))
+  
+  d_summ <- d %>%
+    group_by(variable, t) %>%
+    summarise(mean = mean(value, na.rm = T),
+              sd = sd(value, na.rm = T)) %>%
+    mutate(lcl = mean - sd,
+           ucl = mean + sd) %>%
+    select(variable,t, mean, lcl, ucl)
+  
+  d_summ <- melt(d_summ, id.vars = c("variable","t"))
+  colnames(d_summ)[1] <- "var"
+  d_summ$variable <- factor(d_summ$variable, levels = c("lcl", "mean", "ucl"))
+  
+  burn_in_plot <- ggplot(data = d_summ, aes(x = t, y = value, colour = variable)) + geom_line() +
+    geom_point(size = 0.5, aes(colour = variable)) + 
+    scale_colour_manual(values = c("red4", "green4", "red4")) + 
+    facet_wrap(var ~. , scales = "free") + theme_bw() +
+    labs(title = "Burn-in Period Analysis", x = "Forecast (t)", y = "Value", colour = "") + 
+    theme(legend.position = "bottom")
+  
+  
+  return_list <- list(model_object = a,
+                      model_summary = summary(a),
                       sim_obs = sim_obs,
                       sim_full = sim_list,
                       mean = list(actual = apply(df,2, mean), simulated = apply(sim_obs,2,mean)),
                       sd = list(actual = apply(df,2, sd), simulated = apply(sim_obs,2,sd)),
                       varcov = list(actual = var(df), simulated = var(sim_obs)),
                       corr =list(actual = cor(df), simulated = cor(sim_obs)),
-                      density_plot = density_plot)
+                      density_plot = density_plot,
+                      var_decomp_plot = var_decomp_plot,
+                      burn_in_plot = burn_in_plot)
   return_list
 }
 
+impulse_response_plot <- function(a, ci = 0.95, n.ahead = 10, runs = 100){
+  coeff <- coefficients((a))
+  cross <- expand.grid(impulse = names(coeff), response = names(coeff))
+  ir <- do.call ('rbind', mapply(function(x, y) {
+    d <- irf(x = a, impulse = x, response = y, ci = ci, n.ahead = n.ahead, runs = runs)
+    d <- data.frame(cbind(unlist(d[["irf"]]), unlist(d[["Lower"]]), unlist(d[["Upper"]])))
+    colnames(d) <- c("irf","lcl", "ucl")
+    d$impulse <- x
+    d$response <- y 
+    d$t <- c(1:(n.ahead + 1))
+    d
+  },
+  cross$impulse,  # names from first
+  cross$response, SIMPLIFY = F))
+  
+  ir_long <- melt(ir, id.vars = c("impulse", "response", "t"))
+  ir_long$variable <- factor(ir_long$variable, levels = c("lcl","irf", "ucl"))
+  
+  ggplot(data = ir_long, aes(x = t, y = value)) +
+    geom_line(aes(linetype = variable, colour = variable)) + 
+    scale_colour_manual(values = c("red4", "green4", "red4")) + 
+    scale_linetype_manual(values = c(2,1,2)) + 
+    facet_grid(impulse ~ response , scales = "free") + theme_bw() +
+    labs(title = "Impulse Response", 
+         subtitle = paste("CI =",ci),
+         x = "Forecast (t)", y = "Response", linetype = "", colour = "") + 
+    theme(legend.position = "bottom")
+}
 
-p <- 2
-type <- "restrict"
-restrict_type <- "ser"
-nsim <- 10000
-burn_in_period <- 30
-forecast_start_data <- as.matrix(Canada[(r - p + 1):r,])
-                                 
+
 #check what provides the optimum lag of endogeneous variables
-varselect <- VARselect(y = df)
+varselect <- VARselect(y = Canada)
 print(varselect)
 
-l <- var_simulation(df = Canada, p = 2, nsim = 200, burn_in_period = 20, relative_lags = c(0,1,1,2),
-                    type = "restrict", restrict_type = "ser", thresh = 5)                                 
+restrict_mat <- matrix(c(1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0), nrow=4, ncol=9, byrow=TRUE)
 
+l <- var_simulation(df = Canada, p = 1, nsim = 5000, burn_in_period = 30,
+                    type = "restrict", restrict_type = "ser", thresh = 2, restrict_mat = restrict_mat)                                 
 
-#plotting burn-in period charts
-                                 
-                                 
+impulse_response_plot(a = l$model_object, ci = 0.95, n.ahead = 30, runs = 100)
